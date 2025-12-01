12675a2661ee1d8a3f6fb978f8b196ba57b3c68c
skoletzko <sebastian.koletzko@rub.de>
2025-12-01 13:44:32 +0100

    day01 part 2

--- src/main/java/aoc/advent2025/sk/Day01.java
+++ src/main/java/aoc/advent2025/sk/Day01.java
@@ -31,7 +31,19 @@ public final class Day01 implements Puzzle {
 
     @Override
     public String solvePart2(List<String> input) {
-        return "TODO";
+        SafeDial safeDial = new SafeDial(100, 50);
+
+        for (int i = 0; i < input.size(); i++) {
+            String s = input.get(i);
+            String direction = s.substring(0,1); // "L" | "R"
+            int count = Integer.parseInt(s.substring(1));
+            if (direction.equals("L")) {
+                count = (-1)*count;
+            }
+
+            safeDial.rotateCounting(count);
+        }
+        return Integer.toString(safeDial.getZeroTicks());
     }
 }
 
