7735d310a34705edd3eaa84f62551d19db213dc2
DerMoehre <moehre2507@gmail.com>
2025-12-02 08:22:35 +0100

    day01 part2

--- src/main/java/aoc/advent2025/md/Day01.java
+++ src/main/java/aoc/advent2025/md/Day01.java
@@ -14,12 +14,12 @@ public final class Day01 implements Puzzle {
     private static int zeroCount(List<String> input) {
         int current = 50;
         int zero = 0;
+
         for (int i = 0; i < input.size(); i++){
             String e = input.get(i);
             String turn = e.substring(0,1);
             String value = e.substring(1);
             
-                       
             // left turn
             if (turn.equals("L")) {
                 current = current - (Integer.parseInt(value) % 100);
@@ -44,8 +44,50 @@ public final class Day01 implements Puzzle {
 
     @Override
     public String solvePart2(List<String> input){
-        
-        return "1";
-    }
+        int current = 50;
+        int currentTemp = 50;
+        int rotation = 0;
 
+        for (int i = 0; i < input.size(); i++){
+            String e = input.get(i);
+            String turn = e.substring(0,1);
+            String value = e.substring(1);
+            
+            // placeholder variable for current position
+            currentTemp = current;
+            // calculate the full turns
+            rotation += Integer.parseInt(value) / 100;
+
+            // left turn
+            if (turn.equals("L")) {
+                current -= (Integer.parseInt(value) % 100);
+                if (current < 0) {
+                    current = 100 - Math.abs(current);
+                    // only count the rotations if the starting number was not 0
+                    if (currentTemp != 0){
+                        rotation += 1;
+                    }
+                
+                    currentTemp = current;  
+                }
+            } else {
+                current += (Integer.parseInt(value) % 100);
+                
+                if (current > 100) {
+                    current -= 100;
+                    // only count the rotations if the starting number was not 0
+                    if (currentTemp != 0){
+                        rotation += 1;
+                    }
+                    currentTemp = current;  
+                }
+                }
+            
+            if (current % 100 == 0) {
+                current = 0;
+                rotation += 1;
+            }
+        }
+        return Integer.toString(rotation);
+    }
 }
