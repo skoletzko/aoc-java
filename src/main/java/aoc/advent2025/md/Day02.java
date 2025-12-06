c9290ec40e2779b57ecaf4381cc5496d9437fcc7
Moritz Dahm <94791958+DerMoehre@users.noreply.github.com>
2025-12-06 13:22:46 +0100

    Day02 part2

--- src/main/java/aoc/advent2025/md/Day02.java
+++ src/main/java/aoc/advent2025/md/Day02.java
@@ -2,40 +2,65 @@ package aoc.advent2025.md;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 import aoc.framework.Puzzle;
 
 public final class Day02 implements Puzzle {
 
     @Override
-    public String solvePart1(List<String> input){
+    public String solvePart1(List<String> input) {
         long invalidID = 0;
-        for(int i=0;i<input.size();i++){
+        for (int i = 0; i < input.size(); i++) {
             // generating commaSeparated list out of input string
-            String [] commaSeparated = input.get(i).split(",");
+            String[] commaSeparated = input.get(i).split(",");
             for (String s : commaSeparated) {
                 // generating the number ranges from separated string
-                String [] ranges = s.split("-");
-                for (long r=Long.parseLong(ranges[0]); r<=Long.parseLong(ranges[1]); r++) {
+                String[] ranges = s.split("-");
+                for (long r = Long.parseLong(ranges[0]); r <= Long.parseLong(ranges[1]); r++) {
                     // checking for uneven numbers
                     if (String.valueOf(r).length() % 2 != 0) {
                         continue;
                     }
-                    int halfNumLength = String.valueOf(r).length()/2;
+                    int halfNumLength = String.valueOf(r).length() / 2;
                     String firstHalf = Long.toString(r).substring(0, halfNumLength);
-                    String secondHalf = Long.toString(r).substring(halfNumLength,String.valueOf(r).length());
+                    String secondHalf = Long.toString(r).substring(halfNumLength, String.valueOf(r).length());
 
                     if (firstHalf.equals(secondHalf)) {
                         invalidID += r;
                     }
                 }
             }
-            
+
         }
         return Long.toString(invalidID);
     }
 
     @Override
-    public String solvePart2(List<String> input){
-        return "Part2";
+    public String solvePart2(List<String> input) {
+        long invalidID = 0;
+        for (int i = 0; i < input.size(); i++) {
+            // generating commaSeparated list out of input string
+            String[] commaSeparated = input.get(i).split(",");
+
+            for (String s : commaSeparated) {
+                // generating the number ranges from separated string
+                String[] ranges = s.split("-");
+                for (long r = Long.parseLong(ranges[0]); r <= Long.parseLong(ranges[1]); r++) {
+                    boolean match = false;
+                    for (int j = 1; j <= String.valueOf(r).length() / 2; j++) {
+
+                        String[] parts = Long.toString(r).split("(?<=\\G.{" + j + "})");
+                        if (Arrays.stream(parts).allMatch(str -> str.equals(parts[0]))) {
+                            match = true;
+                            break;
+                        }
+                    }
+                    if (match) {
+                        invalidID += r;
+                    }
+                }
+            }
+        }
+        return Long.toString(invalidID);
     }
 }
