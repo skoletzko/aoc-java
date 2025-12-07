dcdca8583bad369e05fb4569fd2c080ff86f049e
Moritz Dahm <94791958+DerMoehre@users.noreply.github.com>
2025-12-07 08:59:55 +0100

    Day03 part1

--- src/main/java/aoc/advent2025/md/Day03.java
+++ src/main/java/aoc/advent2025/md/Day03.java
@@ -7,13 +7,33 @@ import aoc.framework.Puzzle;
 public final class Day03 implements Puzzle {
 
     @Override
-    public String solvePart1(List<String> input){
-        
-        return "Part1";
+    public String solvePart1(List<String> input) {
+        int result = 0;
+
+        for (String line : input) {
+            int first = Integer.parseInt(line.substring(0, 1));
+            int large = 0;
+
+            for (int i = 1; i < line.length(); i++) {
+                String current = line.substring(i, i + 1);
+                String largeTemp = String.valueOf(first) + String.valueOf(current);
+
+                if (Integer.parseInt(largeTemp) > large) {
+                    large = Integer.parseInt(largeTemp);
+                }
+
+                if (Integer.parseInt(current) > first) {
+                    first = Integer.parseInt(current);
+                }
+
+            }
+            result += large;
+        }
+        return String.valueOf(result);
     }
 
     @Override
-    public String solvePart2(List<String> input){
+    public String solvePart2(List<String> input) {
         return "Part2";
     }
 }
--- src/main/java/aoc/framework/PuzzleRegistry.java
+++ src/main/java/aoc/framework/PuzzleRegistry.java
@@ -40,7 +40,7 @@ public final class PuzzleRegistry {
         registry.register(2023, "sample", 1, aoc.advent2023.sample.Day01::new);
         registry.register(2025, "md", 1, aoc.advent2025.md.Day01::new);
         registry.register(2025, "md", 2, aoc.advent2025.md.Day02::new);
-        registry.register(2025, "md", 3, aoc.advent2025.md.Day02::new);
+        registry.register(2025, "md", 3, aoc.advent2025.md.Day03::new);
         registry.register(2025, "sk", 1, aoc.advent2025.sk.Day01::new);
         registry.register(2025, "sk", 2, aoc.advent2025.sk.Day02::new);
         registry.register(2025, "sk", 3, aoc.advent2025.sk.Day03::new);
