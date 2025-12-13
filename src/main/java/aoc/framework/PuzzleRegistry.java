5b0811e3b4ee614b1354dce4993e1fb0c7e09dfc
skoletzko <sebastian.koletzko@rub.de>
2025-12-13 23:45:16 +0100

    day08 part 1

--- src/main/java/aoc/framework/PuzzleRegistry.java
+++ src/main/java/aoc/framework/PuzzleRegistry.java
@@ -49,6 +49,7 @@ public final class PuzzleRegistry {
         registry.register(2025, "sk", 5, aoc.advent2025.sk.Day05::new);
         registry.register(2025, "sk", 6, aoc.advent2025.sk.Day06::new);
         registry.register(2025, "sk", 7, aoc.advent2025.sk.Day07::new);
+        registry.register(2025, "sk", 8, aoc.advent2025.sk.Day08::new);
 
         return registry;
     }
